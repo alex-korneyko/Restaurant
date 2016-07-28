7a0f565a521363742659df4d4fe8edd677a98a53
alex-korneyko <a.korneyko@gmail.com>
2016-07-28 13:18:55 +0300

    Initial commit

---
+++ hw1queries.sql
@@ -0,0 +1,30 @@
+﻿INSERT INTO service.employee_posts (post_name) VALUES ('Директор');
+INSERT INTO service.employee_posts (post_name) VALUES ('Аминистратор');
+INSERT INTO service.employee_posts (post_name) VALUES ('Официант');
+INSERT INTO service.employee_posts (post_name) VALUES ('Шефповар');
+INSERT INTO service.employee_posts (post_name) VALUES ('Повар');
+INSERT INTO service.employee_posts (post_name) VALUES ('Охрана');
+
+INSERT INTO service.employees (last_name, first_name, date_of_birth, telephone, post_id, salary)
+		VALUES ('Нульпоинтер', 'Ексепшин', '1992-11-24', '(097)444-55-66', (SELECT id FROM service.employee_posts WHERE post_name = 'Директор'), 50000);
+
+INSERT INTO service.employees (last_name, first_name, date_of_birth, telephone, post_id, salary)
+		VALUES ('Иванов', 'Пётр', '1990-10-23', '(097)333-44-55', (SELECT id FROM service.employee_posts WHERE post_name = 'Аминистратор'), 30000);
+
+INSERT INTO service.employees (last_name, first_name, date_of_birth, telephone, post_id, salary)
+		VALUES ('Петров', 'Иван', '1988-05-05', '(067)123-53-45', (SELECT id FROM service.employee_posts WHERE post_name = 'Шефповар'), 30000);
+
+INSERT INTO service.employees (last_name, first_name, date_of_birth, telephone, post_id, salary)
+		VALUES ('Болконский', 'Андрей', '1990-05-14', '(093)453-45-67', (SELECT id FROM service.employee_posts WHERE post_name = 'Повар'), 12000);
+INSERT INTO service.employees (last_name, first_name, date_of_birth, telephone, post_id, salary)
+		VALUES ('Курагина', 'Елена', '1980-10-01', '(093)355-45-36', (SELECT id FROM service.employee_posts WHERE post_name = 'Повар'), 10000);
+
+INSERT INTO service.employees (last_name, first_name, date_of_birth, telephone, post_id, salary)
+		VALUES ('Курагин', 'Василий', '1992-02-29', '(050)753-00-25', (SELECT id FROM service.employee_posts WHERE post_name = 'Официант'), 6000);
+INSERT INTO service.employees (last_name, first_name, date_of_birth, telephone, post_id, salary)
+		VALUES ('Ростова', 'Наталья', '1989-12-31', '(066)453-77-88', (SELECT id FROM service.employee_posts WHERE post_name = 'Официант'), 5000);
+INSERT INTO service.employees (last_name, first_name, date_of_birth, telephone, post_id, salary)
+		VALUES ('Безухов', 'Пьер', '1995-03-08', '(096)123-45-78', (SELECT id FROM service.employee_posts WHERE post_name = 'Официант'), 5000);
+
+INSERT INTO service.employees (last_name, first_name, date_of_birth, telephone, post_id, salary)
+		VALUES ('Денисов', 'Василий', '1975-06-26', '(097)444-55-66', (SELECT id FROM service.employee_posts WHERE post_name = 'Охрана'), 3500);
---
+++ hw1v2.sql
@@ -0,0 +1,95 @@
+﻿CREATE SCHEMA service;
+
+/*Жизненный цикл блюда или заказа*/
+CREATE TYPE service.DISH_ORDER_STATUS AS ENUM('IN_QUEUE', 'IN_PROCESS', 'PREPARED', 'ISSUED');
+
+/*Таблица Должности*/
+CREATE TABLE service.employee_posts (
+	id SMALLSERIAL PRIMARY KEY NOT NULL,
+	post_name CHARACTER VARYING(20) NOT NULL
+);
+
+/*Таблица Сотрудники*/
+CREATE TABLE service.employees (
+	id SMALLSERIAL PRIMARY KEY NOT NULL,
+	last_name CHARACTER VARYING(20) NOT NULL,
+	first_name CHARACTER VARYING(20) NOT NULL,
+	date_of_birth DATE,
+	telephone CHARACTER VARYING(20),
+	post_id INTEGER NOT NULL REFERENCES service.employee_posts(id),
+	salary REAL
+);
+
+/*Таблица ингредиентов*/
+CREATE TABLE service.ingredients (
+	id SMALLSERIAL PRIMARY KEY NOT NULL,
+	ingredient_name CHARACTER VARYING(20) NOT NULL
+);
+
+/*Таблица категорий блюд*/
+CREATE TABLE service.dish_categories (
+	id SMALLSERIAL PRIMARY KEY NOT NULL,
+	category_name CHARACTER VARYING(20) NOT NULL
+);
+
+/*Таблица возможных блюд*/
+CREATE TABLE service.dishes (
+	id SMALLSERIAL PRIMARY KEY NOT NULL,
+	dish_name CHARACTER VARYING(20) NOT NULL,
+	category INTEGER NOT NULL REFERENCES service.dish_categories(id),
+	price DECIMAL,
+	weight DECIMAL
+);
+
+/*Вспомагательная таблица соответствия ингредиенотв блюдам*/
+CREATE TABLE service.dish_composition (
+	dish_id INTEGER NOT NULL REFERENCES service.dishes(id),
+	ingredient_id INTEGER NOT NULL REFERENCES service.ingredients(id),
+	PRIMARY KEY (dish_id, ingredient_id)
+);
+
+/*Склад*/
+CREATE TABLE service.warehouse (
+	id SMALLSERIAL PRIMARY KEY NOT NULL,
+	ingredient_id INTEGER NOT NULL REFERENCES service.ingredients(id),
+	amount DECIMAL NOT NULL
+);
+
+/*Заказы*/
+CREATE TABLE service.orders (
+	id SERIAL PRIMARY KEY NOT NULL,
+	employee_id INTEGER NOT NULL REFERENCES service.employees(id),
+	desk INTEGER NOT NULL,
+	status service.DISH_ORDER_STATUS NOT NULL,
+	order_date DATE
+);
+
+/*Вспомагательная таблица соответствия блюд заказам*/
+CREATE TABLE service.order_composition (
+	order_id INTEGER NOT NULL REFERENCES service.orders(id),
+	dish_id INTEGER NOT NULL REFERENCES service.dishes(id),
+	PRIMARY KEY (order_id, dish_id)
+);
+
+/*Таблица различных меню*/
+CREATE TABLE service.menu (
+	id SMALLSERIAL PRIMARY KEY NOT NULL,
+	menu_name CHARACTER VARYING(20) NOT NULL
+);
+
+/*Вспомагательная таблица соответствия блюд меню*/
+CREATE TABLE service.menu_composition (
+	menu_id INTEGER NOT NULL REFERENCES service.menu(id),
+	dish_id INTEGER NOT NULL REFERENCES service.dishes(id),
+	PRIMARY KEY (menu_id, dish_id)
+);
+ 
+/*Таблица заказаных блюд и их статус*/
+CREATE TABLE service.kitchen (
+	id SERIAL NOT NULL PRIMARY KEY,
+	dish_id INTEGER NOT NULL REFERENCES service.dishes(id),
+	employee_id INTEGER NOT NULL REFERENCES service.employees(id),
+	order_id INTEGER NOT NULL REFERENCES service.orders(ID),
+	status_of_dish service.DISH_ORDER_STATUS NOT NULL,
+	date_of_cooked DATE
+);
