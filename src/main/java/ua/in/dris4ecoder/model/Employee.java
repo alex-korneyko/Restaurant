8ad8b52baee05abdef734837db93cbc1736aa0fc
alex-korneyko <a.korneyko@gmail.com>
2016-08-02 20:48:16 +0300

    Partially realized JdbcEmployeeDao

--- src/main/java/ua/in/dris4ecoder/model/Employee.java
+++ src/main/java/ua/in/dris4ecoder/model/Employee.java
@@ -1,7 +1,71 @@
 package ua.in.dris4ecoder.model;
 
+import java.util.Date;
+
 /**
  * Created by Alex Korneyko on 29.07.2016 18:15.
  */
 public class Employee {
+
+    private int id;
+    private String lastName;
+    private String firstName;
+    private Date dateOfBirth;
+    private String telephone;
+    private int postId;
+    private double salary;
+
+    public Employee(String lastName, String firstName, int postId) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.postId = postId;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public int getPostId() {
+        return postId;
+    }
+
+    public void setPostId(int postId) {
+        this.postId = postId;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
 }
--- src/main/java/ua/in/dris4ecoder/model/dao/jdbc/JdbcEmployeeDao.java
+++ src/main/java/ua/in/dris4ecoder/model/dao/jdbc/JdbcEmployeeDao.java
@@ -1,11 +1,15 @@
 package ua.in.dris4ecoder.model.dao.jdbc;
 
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 import ua.in.dris4ecoder.model.Employee;
 import ua.in.dris4ecoder.model.KitchenProcess;
 import ua.in.dris4ecoder.model.OrderDishStatus;
 import ua.in.dris4ecoder.model.dao.RestaurantDao;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Date;
 import java.util.List;
 
@@ -23,16 +27,36 @@ public class JdbcEmployeeDao implements RestaurantDao<Employee> {
     @Override
     public void addItem(Employee item) {
 
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(
+                             "INSERT INTO service.employees (last_name, first_name, date_of_birth, telephone, post_id, salary) VALUES (?, ?, ?, ?, ?, ?)")) {
+            statement.setString(1, item.getLastName());
+            statement.setString(2, item.getFirstName());
+            statement.setDate(3, (java.sql.Date) item.getDateOfBirth());
+            statement.setString(4, item.getTelephone());
+            statement.setInt(5, item.getPostId());
+            statement.setDouble(6, item.getSalary());
+            statement.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     @Override
     public void removeItemById(int id) {
 
+        try (Connection connection = dataSource.getConnection();
+        PreparedStatement statement = connection.prepareStatement("DELETE FROM service.employees WHERE id = ?")) {
+            statement.setInt(1, id);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void removeItemByName(String name) {
-
+        throw new NotImplementedException();
     }
 
     @Override
